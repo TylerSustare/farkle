2a85ef2a4bc13e620a13da7e30e71b45afd64302
Tyler Sustare <tylersustare@gmail.com>
2022-01-13 21:58:36 -0800

    Add game utility functions

--- src/utils/board.ts
+++ src/utils/board.ts
@@ -1,9 +1,9 @@
-import { BoardState } from './types';
+import { BoardState, Moves } from './types';
 
 export function formattedBoard(board: BoardState): string {
   return board
     .map((cell, i) => {
-      let line = cell ? ` ${cell} |` : ' |';
+      let line = cell ? ` ${cell} |` : '  |';
       if (i % 3 === 2) {
         line = line.slice(0, -1);
         if (i < board.length - 1) line += '\n--- --- ---\n';
@@ -12,3 +12,21 @@ export function formattedBoard(board: BoardState): string {
     })
     .join('');
 }
+
+export const isEmpty = (state: BoardState): boolean => {
+  return state.every((cell) => cell === null);
+};
+
+export const isFull = (state: BoardState): boolean => {
+  return state.every((cell) => cell);
+};
+
+export const availableMoves = (state: BoardState): Moves[] => {
+  const moves: Moves[] = [];
+  state.forEach((cell, index) => {
+    if (cell === null) {
+      moves.push(index as Moves);
+    }
+  });
+  return moves;
+};
--- src/utils/tests/board.test.ts
+++ src/utils/tests/board.test.ts
@@ -1,16 +1,98 @@
-import { formattedBoard } from '../board';
+import { availableMoves, formattedBoard, isEmpty, isFull } from '../board';
 import { BoardState } from '../types';
 
 describe('board tests', () => {
-  it('should return the correct formatted board', () => {
-    const board: BoardState = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'];
-    const fmt = formattedBoard(board);
-    expect(fmt).toBe(
-      ' x | x | x \n' +
-        '--- --- ---\n' +
-        ' x | x | x \n' +
-        '--- --- ---\n' +
-        ' x | x | x ',
-    );
+  describe('formattedBoard', () => {
+    it('should return the correct formatted board', () => {
+      const board: BoardState = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+      const fmt = formattedBoard(board);
+      expect(fmt).toBe(
+        ' x | o | x \n' +
+          '--- --- ---\n' +
+          ' o | x | o \n' +
+          '--- --- ---\n' +
+          ' x | o | x ',
+      );
+    });
+
+    it('should print an empty board', () => {
+      const board: BoardState = [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+      ];
+      expect(formattedBoard(board)).toBe(
+        '  |  |  \n' +
+          '--- --- ---\n' +
+          '  |  |  \n' +
+          '--- --- ---\n' +
+          '  |  |  ',
+      );
+    });
+  });
+
+  describe('isEmpty', () => {
+    it('should return true for an empty board', () => {
+      const board: BoardState = [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+      ];
+      expect(isEmpty(board)).toBe(true);
+    });
+
+    it('should return false for a non-empty board', () => {
+      const board: BoardState = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'];
+      expect(isEmpty(board)).toBe(false);
+    });
+  });
+  describe('isFull', () => {
+    it('should return true for a full board', () => {
+      const board: BoardState = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'];
+      expect(isFull(board)).toBe(true);
+    });
+
+    it('should return false for a non-full board', () => {
+      const board: BoardState = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', null];
+      expect(isFull(board)).toBe(false);
+    });
+  });
+
+  describe('availableMoves', () => {
+    it('should return all available moves', () => {
+      const board: BoardState = [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+      ];
+      expect(availableMoves(board)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+    });
+
+    it('should return no available moves', () => {
+      const board: BoardState = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+      expect(availableMoves(board)).toEqual([]);
+    });
+    it('should return only one available move', () => {
+      const board: BoardState = ['x', 'x', 'o', 'x', 'o', 'x', 'x', 'o', null];
+      expect(availableMoves(board)).toEqual([8]);
+    });
   });
 });
